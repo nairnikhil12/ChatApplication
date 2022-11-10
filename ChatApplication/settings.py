6f6c82009957ca581f8f58dbf997ac4cb0a98352
Darkpsychic <nairnikhil2001@gmail.com>
2022-11-10 10:44:32 +0530

    Install and setup channels library

--- ChatApplication/settings.py
+++ ChatApplication/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -67,7 +68,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ChatApplication.wsgi.application"
+# WSGI_APPLICATION = "ChatApplication.wsgi.application"
+ASGI_APPLICATION = "ChatApplication.asgi.application"
 
 
 # Database
